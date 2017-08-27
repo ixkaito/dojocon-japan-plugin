3b65f34aeb6fc04958b2011fe1a34a803c574d7a
Kite <ixkaito@gmail.com>
2017-08-28 05:34:38 +0900

    initial commit

---
+++ dojocon-japan.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: DojoCon Japan
+Plugin URI: http://dojocon.coderdojo.jp/
+Descript: DojoCon Japan must-use plugin
+Version: 1.0.0
+Author: Kite
+Author URI: https://profiles.wordpress.org/ixkaito
+License: GPLv2 or later
+Text Domain: dojoconjapan
+*/
+
+define( 'DOJOCONJAPAN_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
